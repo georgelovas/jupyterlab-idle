049f61e0f04d67e0923ba5246bdd1af7744037db
george lovas <george.lovas@adyne.com>
2020-07-01 11:58:24 -0400

    Initial Checkin....

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application'; 
+
+const idleCheckPlugin: JupyterFrontEndPlugin<void> = {
+  id: '@adyne/general:idle-check',
+  activate: activateIdleCheck,
+  autoStart: true
+};
+
+function activateIdleCheck(
+  app: JupyterFrontEnd
+): void {
+        console.log(`JupyterLab extension autologout is activated`);
+        var timeout = 300 * 1000;  // 5 min
+        var timeoutId = 0;
+        var lastTrigger = 0;
+
+        function startTimer() {
+            timeoutId = window.setTimeout(doInactive, timeout);
+        }
+
+        function resetTimer() {
+            // timer reset every 10 sec on event trigger
+            if (Date.now() - lastTrigger > 10000) {
+                console.log(`logout timer reset`);
+                lastTrigger = Date.now();
+                window.clearTimeout(timeoutId);
+                startTimer();
+            }
+        }
+
+        function doInactive() {
+            console.log(`auto logout triggered`);
+            window.location.href = "/hub/logout/idle";
+        }
+
+        function setupTimers() {
+            document.addEventListener("mousemove", resetTimer);
+            document.addEventListener("mousedown", resetTimer);
+            document.addEventListener("keypress", resetTimer);
+            document.addEventListener("touchmove", resetTimer);
+     
+            startTimer();
+        }
+
+        setupTimers();
+    }
+const plugins: JupyterFrontEndPlugin<any>[] = [idleCheckPlugin];
+export default plugins;
+
